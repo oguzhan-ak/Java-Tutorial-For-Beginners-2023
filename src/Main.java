3cf9cd8e9a116ae732077a751d2e0fc7a6784dd3
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-14 16:27:54 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-
-            System.out.println("i = " + i);
-
-            System.out.println("i = " + i);
-
-            System.out.println("i = " + i);
-        }
+        //primitives
+        int number1 = 20;
+        int number2 = 20;
+        System.out.println(number1 + number2);
+        double pi = 3.14;
+        System.out.println(pi);
+        boolean isOkay = true;
+        System.out.println(isOkay);
+        char a = 'A';
+        System.out.println(a);
     }
 }
