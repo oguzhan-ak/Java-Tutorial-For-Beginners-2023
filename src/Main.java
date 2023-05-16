18389ae0fe4c176a768c9cfd33b3f500dd5ccf26
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-16 21:29:51 +0300

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
-import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Point pointA = new Point(10, 10);
-        pointA.move(12, 12);
-        System.out.println(pointA);
+        // Arrays
+        int[] numbers = new int[3];
+        numbers[0] = 12;
+        numbers[1] = 22;
+        numbers[2] = 32;
+        System.out.println(Arrays.toString(numbers));
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println(numbers.length);
+
+        double[] numbers2 = {2, 3, 4, 5, 6};
+        numbers2[3] = 3;
+        System.out.println(Arrays.toString(numbers2));
     }
 }
