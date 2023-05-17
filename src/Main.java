fb6efa8286e23ca45cfbd949bfe41dbdcba08678
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-05-17 18:45:49 +0300

    loop

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        // Arrays
-        int[] numbers = new int[3];
-        Arrays.fill(numbers, 2);
-        System.out.println(Arrays.toString(numbers));
-
-        String[] names = new String[2];
-        names[0] = "Ali";
-        System.out.println(Arrays.toString(names));
-        Arrays.fill(names, "Ayşe");
-        System.out.println(Arrays.toString(names));
+        // Loop Arrays
+        String[] names = {"Ali", "Mehmet", "Ayse"};
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
